073306ec1857a5c835ae79a5f998a138758ac7a8
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-02-11 03:24:19 +0500

    Complete 02_repatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    if (num < 0) {
+        return `ERROR`;
+    }
 
+    else if (!string) {
+        return ``;
+    }
+
+    let result = ``;
+    while (num >= 1) {
+        result += string;
+        num--;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
