1a02989abd35aa8eae76b6c33ea8dfbf6d0e5866
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-02-11 21:22:43 +0500

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  fahrenheit = Number.parseFloat(fahrenheit);
+
+  if (isNaN(fahrenheit))
+    return `ERROR`;
+
+  let celcius = ((fahrenheit - 32) * 5/9);
+
+  return parseFloat(celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+
+  celcius = Number.parseFloat(celcius);
+
+  if (isNaN(celcius))
+    return `ERROR`;
+
+  let fahrenheit = (celcius * 9/5) + 32;
+
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
