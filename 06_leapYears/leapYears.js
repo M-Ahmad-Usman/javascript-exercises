4f294ecb1da605fb2a061e9aa395da47b84b2534
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-02-11 19:31:29 +0500

    Complete 05_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    year = Number.parseInt(year);
+    
+    if (year == undefined)
+        return `ERROR`;
 
+    if (year % 4 == 0) {
+        
+        if (year % 400 != 0 && year % 100 == 0)
+            return false;
+        else
+            return true;
+    }
+    else
+        return false;
 };
 
 // Do not edit below this line
