7a5de9a6aae05d5e5ce089f72ae7ea94cf4db7d8
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-02-11 13:57:30 +0500

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...number) {
+    return array.filter((value)=> {
+        for (let i = 0; i < number.length; i++) {
+            if (number[i] === value)
+                return false;
+        }
+        return true;
+    });
 };
 
 // Do not edit below this line
