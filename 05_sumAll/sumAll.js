36e26c5298734806576cef166d38c638cc8ed2f5
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-02-11 19:15:11 +0500

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    
+    // Error handling
+    if (num1 < 0 || num2 < 0)
+        return `ERROR`;
+    else if (!Number.isInteger(num1) || !Number.isInteger(num2))
+        return `ERROR`;
+    
+    // Calculating the sum
+    let result = 0;
+    if (num1 < num2) {
+        while (num2 >= num1) {
+            result += num2;
+            num2--;
+        }
+    }
+    else {
+        while (num1 >= num2) {
+            result += num1;
+            num1--;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
