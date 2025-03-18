19a99eaf8a4d899af12e44b4e2104e50210ac77a
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-03-18 11:12:05 +0500

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Filter non alphanumerical characters from parameter
+    // Convert the array of alphanumerical characters into string 
+    // Convert the string into lowercase
+    let filteredStr = str
+    .match(/[A-Za-z0-9]/g)
+    .toString()
+    .toLowerCase();
 
+    let reversedStr = filteredStr
+    .split("")
+    .reverse()
+    .join("");
+
+    return reversedStr === filteredStr;
 };
 
 // Do not edit below this line
