ddc3c9682b59920e4410f1838e00d85677750584
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-03-18 11:39:48 +0500

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0)
+        return `OOPS`;
 
+    if (n == 0)
+        return 0;
+    if (n == 1) 
+        return 1;
+    if (n == 2)
+        return 1;
+    else {
+        let first = 0, second = 1;
+        let sum = first + second;
+        while (n > 2) {
+            first = second;
+            second = sum;
+            sum = first + second;
+            n--;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
