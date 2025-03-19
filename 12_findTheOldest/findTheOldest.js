754b889d724f95be819e7d5ab2cb77fc2e8d39c7
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-03-20 00:41:47 +0500

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people.reduce((currentPerson, nextPerson) => {
+        let DOB = "yearOfBirth";
+        let DOT = "yearOfDeath";
 
+        // If persons's year of death is undefined then get the current year
+        // The precedence of ?? is less than -, that's why it is enclosed in paranthesis
+        let currentPersonAge = 
+        (currentPerson[DOT] ?? new Date().getFullYear()) - currentPerson[DOB];
+        
+        let nextPersonAge = 
+        (nextPerson[DOT] ?? new Date().getFullYear()) - nextPerson[DOB];
+
+        return (currentPersonAge > nextPersonAge)? currentPerson: nextPerson;
+
+    }, people[0])
+
+    return oldest;
+    
 };
 
 // Do not edit below this line
